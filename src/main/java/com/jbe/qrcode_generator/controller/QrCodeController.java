5a146a02b78bf7c6f0c72f53b509ea79a814f97a
JBEstevan <jbe.estevan@gmail.com>
2026-09-12 23:50:12 -0300

    feat: integra o serviço de geração ao controller

--- src/main/java/com/jbe/qrcode_generator/controller/QrCodeController.java
+++ src/main/java/com/jbe/qrcode_generator/controller/QrCodeController.java
@@ -8,15 +8,28 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.jbe.qrcode_generator.dto.QrCodeGenerateRequest;
 import com.jbe.qrcode_generator.dto.QrCodeGenerateResponse;
+import com.jbe.qrcode_generator.service.QrCodeGeneratorService;
 
 @RestController
 @RequestMapping("/qrcode")
 
 public class QrCodeController {
 
+	private final QrCodeGeneratorService qrCodeGeneratorService;
+
+	public QrCodeController(QrCodeGeneratorService qrCodeService) {
+		this.qrCodeGeneratorService = qrCodeService;
+	}
+
 	@PostMapping
 	public ResponseEntity<QrCodeGenerateResponse> generate(@RequestBody QrCodeGenerateRequest request) {
-		return null;
+		try {
+			QrCodeGenerateResponse response = this.qrCodeGeneratorService.generateAndUploadQrCode(request.text());
+			return ResponseEntity.ok(response);
+		} catch (Exception e) {
+			return ResponseEntity.internalServerError().build();
+		}
+
 	}
 
 }
